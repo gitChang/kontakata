914d114aec90683c43a33f3fd87b466300dfd7d5
Charlie GP <whchaz1027@gmail.com>
2015-06-07 03:40:28 +0800

    adding BDD/Test.

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -1,29 +1,17 @@
 module MyApp
   class DiagnosticMiddleware
-    FILENAME = File.join(Rails.root, 'log', 'test.log')
-
+    FILENAME = 'diagnostic.txt'
+ 
     def initialize(app)
       @app = app
     end
-
+ 
     def call(env)
       return @app.call(env)
     rescue StandardError => e
       trace = e.backtrace.select{ |l|l.start_with?(Rails.root.to_s) }.join("\n    ")
       msg = "#{e.class}\n#{e.message}\n#{trace}\n"
-      File.open(FILENAME, 'a') { |f| 
-        f.write "\n"
-        f.write "\n"
-        f.write " ---   BACKTRACE   --- \n"
-        f.write "\n"
-
-        f.write msg 
-
-        f.write "\n"
-        f.write " ---   BACKTRACE   --- \n"
-        f.write "\n"
-        f.write "\n"
-      }
+      File.open(FILENAME, 'a') { |f| f.write msg }
       raise e
     end
   end
